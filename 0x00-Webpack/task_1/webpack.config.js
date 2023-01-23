a6ae670f775a5acaba359888ccbe4bf007e477f9
Hezbon12 <otienohezbon12@gmail.com>
2023-01-23 23:39:45 +0300

    webpack

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public'
-  },
-  mode: 'production',
+    mode: "production",
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
 };
